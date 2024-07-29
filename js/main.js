9960fa61344c5f5fd0a8fe574e4568710814037c
y-syr <ysyr07@gmail.com>
2024-07-29 23:10:37 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function swapStation() {
 function submitForm() {
     const departure = encodeURIComponent(document.getElementById('departure').value);
     const arrival   = encodeURIComponent(document.getElementById('arrival').value);
-    const currenDeta = new Data();
+    const currentDate = new Data();
     const formattedDate = `${currentDate.getFullYear()}${('0' + (currentDate.getMonth() + 1)).slice(-2)}${('0' + currentDate.getDate()).slice(-2)}${('0' + currentDate.getHours()).slice(-2)}${('0' + currentDate.getMinutes()).slice(-2)}`;
     const baseUrl = "https://transfer.navitime.biz/tokyubus/pc/extif/TransferIF";
     const url = `${baseUrl}?orvName=${departure}&dnvName=${arrival}&date=${formattedDate}&basis=1&sort=2&wSpeed=normal`;
